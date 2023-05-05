4d27048d712c95a1ab53466f4d409862b7036d1c
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2023-05-05 13:54:18 +0300

    UI Manager and screens for adventurers and gm

--- Assets/Scripts/OnlineGameManager.cs
+++ Assets/Scripts/OnlineGameManager.cs
@@ -6,20 +6,17 @@ using UnityEngine;
 public class OnlineGameManager : MonoBehaviourPun
 {
     public const string NETWORK_PLAYER_PREFAB_NAME = "NetworkPlayerObject";
-    [SerializeField] private Transform[] spawnPoints;
+    public static OnlineGameManager Instance;
 
-    private PlayerController localPlayerController;
-    
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
     void Start()
     {
-        if (PhotonNetwork.IsConnectedAndReady)
-        {
-            localPlayerController =
-                PhotonNetwork.Instantiate(NETWORK_PLAYER_PREFAB_NAME, 
-                        spawnPoints[PhotonNetwork.LocalPlayer.ActorNumber - 1].position, 
-                        spawnPoints[PhotonNetwork.LocalPlayer.ActorNumber - 1].rotation)
-                    .GetComponent<PlayerController>();
-        }
     }
 
 }
