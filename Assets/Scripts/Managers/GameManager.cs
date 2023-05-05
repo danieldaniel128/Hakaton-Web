2136a9b9811fd57181b9a227e83ac33336033cea
EladReuven <102731503+EladReuven@users.noreply.github.com>
2023-05-05 21:24:31 +0300

    sds

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,8 @@
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : SingletonPUN<GameManager>
@@ -10,15 +12,31 @@ public class GameManager : SingletonPUN<GameManager>
 
     //UI
     [SerializeField] GameObject NormieUI;
+    [SerializeField] TextMeshProUGUI PlayerIDText;
     [SerializeField] GameObject GMUI;
+
+    //Players
+    bool[] playersJoinedRoom;
+    List<Player> players= new List<Player>();
 
     int currentPlayerTurn;
     int playerID;
 
     private void Start()
     {
-        
+        if(playersJoinedRoom == null)
+        {
+            playersJoinedRoom = new bool[PhotonNetwork.PlayerList.Length];
+        }
+
+        playerID = PhotonNetwork.LocalPlayer.ActorNumber;
+        PlayerIDText.text = "ID: " + playerID;
+        playersJoinedRoom[playerID - 1] = true;
+        print("playerslist length: " + PhotonNetwork.PlayerList.Length);
     }
+
+
+    
 
     public enum CombatType
     {
